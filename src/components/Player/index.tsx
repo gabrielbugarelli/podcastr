9e7dc229d9e6130f392f4713067fd5e42206fdce
Gabriel Fonseca <gabrielfonsecaoficial@gmail.com>
2022-03-28 15:28:44 -0400

    feat estilização do component App

---
+++ src/components/Player/index.tsx
@@ -0,0 +1,8 @@
+import styles from './styles.module.scss';
+
+export const Player = () => {
+  return (
+    <div className={styles.playerContainer}>
+    </div>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,8 @@
 import type { NextPage } from 'next'
-import { Header } from '../components/Header'
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <Header />
-    </div>
+    <h1>index</h1>
   )
 }
 
