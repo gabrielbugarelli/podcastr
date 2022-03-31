dffa95982625b19179669167b5b77826cbed9a2e
Gabriel <gabrielfonsecaoficial@gmail.com>
2022-03-31 18:59:25 -0400

    feat: listagem dos Posdcasts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,25 @@
 import type { GetStaticProps } from 'next'
+import { format, parseISO } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 import { getAllPodcasts } from '../service/homeService'
+import { convertDurationToTimeString } from '../utils/convertionDurationToTimeString';
 
-type PodcastType = {
-  episodes: [
-    {
-      id: string,
-      title: string,
-      members: string,
-      published_at: string,
-      thumbnail: string,
-      description: string,
-      file: {
-        url: string,
-        type: string,
-        duration: number
-      }
-    }
-  ]
+type Episode = {
+  id: string;
+  title: string;
+  thumbnail: string;
+  members: string;
+  duration: number;
+  durationAsString: string;
+  url: string;
+  publishedAt: string;
 }
 
-const Home = ({episodes}: PodcastType) => {
+type HomeProps = {
+  episodes: Episode[];
+}
+
+const Home = ({episodes}: HomeProps) => {
 
   return (
     <>
@@ -38,10 +38,23 @@ export default Home;
 
 export const getStaticProps: GetStaticProps = async () => {
   const data = await getAllPodcasts();
-  
+
+  const episodes = data.map(episode => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', { locale: ptBR }),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+      url: episode.file.url
+    }
+  });
+
   return {
     props: {
-      episodes: data
+      episodes
     },
 
     revalidate: 60 * 60 * 8
--- src/service/homeService.ts
+++ src/service/homeService.ts
@@ -2,9 +2,29 @@ import axios from 'axios';
 
 const url = 'http://localhost:3333/episodes';
 
-export const getAllPodcasts = () => {
+type PodcastType = {
+  id: string,
+  title: string,
+  members: string,
+  published_at: string,
+  thumbnail: string,
+  description: string,
+  file: {
+    url: string,
+    type: string,
+    duration: number
+  }
+}
+
+export const getAllPodcasts = (): Promise<PodcastType[]> => {
   return new Promise( async (resolve, _) => {
-    const response = await axios.get(url);
+    const response = await axios.get(url, {
+      params: {
+        _limit: 12,
+        _sort: 'published_at',
+        _order: 'desc'
+      }
+    });
     resolve(response.data);
   })
 }
